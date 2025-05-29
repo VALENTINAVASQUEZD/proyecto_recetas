78099a36d93368bcdb9fef7451a765eee7e7a77b
santiago <santiago101ramoss@gmail.com>
2025-05-28 21:10:07 -0500

    cambios

--- lib/services/appwrite_service.dart
+++ lib/services/appwrite_service.dart
@@ -8,31 +8,24 @@ class AppwriteService {
   static final AppwriteService _instance = AppwriteService._internal();
   factory AppwriteService() => _instance;
   AppwriteService._internal();
-  
+
   late Client client;
   late Account account;
   late Databases databases;
   late Storage storage;
-  
+
   void initialize() {
-    if (AppwriteConstants.projectId == '68363bfe000210ecc4f3') {
-      print('Appwrite no configurado - usando solo almacenamiento local');
-      return;
-    }
-    
     client = Client()
         .setEndpoint(AppwriteConstants.endpoint)
         .setProject(AppwriteConstants.projectId);
-    
+
     account = Account(client);
     databases = Databases(client);
     storage = Storage(client);
   }
-  
+
   Future<Document?> syncUser(UserModel user) async {
     try {
-      if (AppwriteConstants.projectId == '68363bfe000210ecc4f3') return null;
-      
       final document = await databases.createDocument(
         databaseId: AppwriteConstants.databaseId,
         collectionId: AppwriteConstants.usersCollectionId,
@@ -45,11 +38,9 @@ class AppwriteService {
       return null;
     }
   }
-  
+
   Future<Document?> syncRecipe(Recipe recipe) async {
     try {
-      if (AppwriteConstants.projectId == '68363bfe000210ecc4f3') return null;
-      
       final document = await databases.createDocument(
         databaseId: AppwriteConstants.databaseId,
         collectionId: AppwriteConstants.recipesCollectionId,
